d4778a688525387371fc43c9de8e537160fb269e
max <maxfy@gmail.com>
2021-06-08 16:06:28 +0300

    dock upd

--- write_channels.py
+++ write_channels.py
@@ -6,6 +6,7 @@ import MySQLdb
 from rq import Worker, Queue, Connection
 from methods.connection import get_redis, get_cursor
 
+r = get_redis()
 
 def write_channels(data):
     """Write channels into database (table channels)
@@ -38,8 +39,6 @@ def write_channels(data):
 
 
 if __name__ == '__main__':
-    time.sleep(5)
-    r = get_redis()
     q = Queue('write_channels', connection=r)
     with Connection(r):
         worker = Worker([q], connection=r,  name='write_channels')
